2ea108c2d1dad7601ca0b56672d37b2abb959340
Jefferson Souza Jr <hbrcomercialssa@gmail.com>
2025-03-24 23:34:11 -0300

    commit inicial projeto mentoria Java

---
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualJuniorApplication.java
@@ -0,0 +1,13 @@
+package jdev.mentoria.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualJuniorApplication.class, args);
+	}
+
+}
